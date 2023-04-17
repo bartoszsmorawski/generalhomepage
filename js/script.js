1835d86e40b8da0810190d0ea3eb7a2d6db6c2d8
Bartosz Smorawski <holzturbs@gmail.com>
2023-04-17 13:52:00 +0200

    change js-function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
-const welcome = () =>  {
-    console.log("Witajcie rekrutatorzy zapraszam do sprawdzenia mojej strony 🙂");
+{
+    const welcome = () => {
+        console.log("Witajcie rekrutatorzy zapraszam do sprawdzenia mojej strony 🙂");
+    }
+
+    const toggleBackground = () => {
+        const body = document.body;
+        const nextColorName = document.querySelector(".js-nextColorName");
+
+        body.classList.toggle("body--dark");
+        nextColorName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny"
+    };
+
+    const init = () => {
+        const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+        changeBackgroundButton.addEventListener("click", toggleBackground);
+
+        welcome();
+    };
+
+    init();
+
 }
-
-const onChangeBackgroundClick =() => {
-    body.classList.toggle("body--dark");
-    nextColorName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny"};
-
-welcome();
-
-
-let body = document.body;
-let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-let nextColorName = document.querySelector(".js-nextColorName");
-
-changeBackgroundButton.addEventListener("click", onChangeBackgroundClick);
-
-
-
